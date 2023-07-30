685eb58f47887d798ab48e89de96d68c1fa06bd4
Md. Hasan Mia <hasanrafi69@gmail.com>
2023-07-30 12:27:10 +0600

    added react-navigation

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './Home';
 
-const Stack = createNativeStackNavigator();
+import Home from './Home';
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Home/>
   );
 }
 
